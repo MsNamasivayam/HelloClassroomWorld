b7b2456f29066ada56e6544f71bec6728cc7e868
Nandhini Namasivayam <nnamasivayam@menloschool.org>
2026-08-18 10:52:30 -0700

    Moved tests and fixed yaml

--- tests/hello_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from hello import *
-
-class TestHello(unittest.TestCase):
-
-    def test_say_hello(self):
-        """Tests the hello output"""
-        self.assertEqual(say_hello(), "Hello Classroom!")
